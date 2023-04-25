d38a9da1f8757beeded0e5a63d528a431a79cb96
Gyuseong Moon <ngng-s@hanmail.net>
2023-04-25 16:50:55 +0900

    [#0]Database with Sequelize

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const path = require('path');
 const session = require('express-session');
 const nunjucks = require('nunjucks'); 
 const dotenv = require('dotenv');
+const {sequelize} = require('./models');
 
 // 자바스크립트는 위에서부터 읽기 때문에 여기는 아직 process.env를 모름
 dotenv.config(); // process.env 파일을 읽어옴.
@@ -17,7 +18,13 @@ nunjucks.configure('views', {
     express: app,
     watch: true,
   });
-
+sequelize.sync({force:false})
+  .then(() => {
+    console.log('데이터베이스 연결 성공')
+  })
+  .catch((err) => {
+    console.error(err);
+  })
 app.use(morgan('dev'));  // 로깅을 개발모드로
 app.use(express.static(path.join(__dirname, 'public')));    // public 폴더는 프론트가 접근 할 수 있도록 허용
 app.use(express.json());
--- models/user.js
+++ models/user.js
@@ -2,10 +2,55 @@ const Sequelize = require('sequelize');
 
 class User extends Sequelize.Model {
     static initiate(sequelize) {
-
+        User.init({
+            email:{
+                type: Sequelize.STRING(40),
+                allowNull: true,    // 카카오톡 로그인 기능 시 없어도 된다
+                unique: true,   // 단 기입된다면 중복되면 안된다
+            },
+            nick: {
+                type: Sequelize.STRING(15),
+                allowNull: false,
+            },
+            password: {
+                type: Sequelize.STRING(100),
+                allowNull: true,
+            },
+            provider: {
+                type: Sequelize.ENUM('local', 'kakao'),
+                allowNull : false,
+                defaultValue: 'local'
+            },
+            snsId: {
+                type: Sequelize.STRING(30),
+                allowNull: true,
+            }
+        }, {
+            sequelize,
+            timestamps: true, // createdAt, updatedAt 을 비교함
+            underscored: false,
+            modelName: 'User',
+            tableName: 'users',
+            paranoid: true, // deletedAt : 유저삭제일이 기입되면 데이터를 비활성화(soft delete)
+            charset: 'utf8',
+            collate : 'utf8_general_ci',
+            })
     }
 
-    static associate(db) {}
+    static associate(db) {
+        db.User.hasMany(db.Post);
+        db.User.belongsToMany(db.User, {    // 팔로워
+            foreignKey: 'followingId',  // 찾을 사람의 ID를 먼저 찾아야
+            as: 'Followers',            // 팔로워를 찾을 수 있다
+            through: 'Follow'
+
+        })
+        db.User.belongsToMany(db.User, {    // 팔로잉
+            foreignKey: 'followerId',
+            as: 'Followings',
+            through: 'Follow'
+        })
+    }
 }
 
 module.exports = User;
