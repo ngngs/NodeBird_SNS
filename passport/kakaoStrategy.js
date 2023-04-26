b984da1b88a09bd53678231350910e5df85ff0e5
Gyuseong Moon <ngng-s@hanmail.net>
2023-04-26 16:02:46 +0900

    [#3] Login with Kakao

--- passport/kakaoStrategy.js
+++ passport/kakaoStrategy.js
@@ -1,1 +1,30 @@
-module.exports = () => {};
+const passport = require('passport');
+const {Strategy : KakaoStrategy} = require('passport-kakao');
+const User = require('../models/user');
+module.exports = () => {
+    passport.use(new KakaoStrategy({
+        clientID: process.env.KAKAO_ID, // KAKAO Developers에서 REST API를 받아올 수 있다.
+        callbackURL : '/auth/kakao/callback',   // Kakao 로그인 Redirect URL에 입력해줘야 함
+    }, async (accessToken, refreshToken, profile, done) => {    // profile은 카카오가 제공하는 거에 따라 다르기 때문에 콘솔을 찍어보자
+        console.log('profile', profile);
+        try{
+            const exUser = await User.findOne({
+                where: { snsId: profile.id, provider: 'kakao'}
+            });
+            if (exUser) {
+                done(null, exUser);
+            } else {
+                const newUser = await User.create({
+                    email: profile._json?.kakao_account?.email,
+                    nick: profile.displayName,
+                    snsId : profile.id,
+                    provider: 'kakao',
+                })
+                done(null, newUser);
+            }
+        } catch (error) {
+            console.error(error);
+            done(error)
+        }
+    }));
+};
--- routes/auth.js
+++ routes/auth.js
@@ -8,4 +8,12 @@ router.post('/join', isNotLoggedIn, join);
 router.post('/login', isNotLoggedIn, login);
 router.get('/logout', isLoggedIn, logout);
 
+// 카카오 로그인
+router.get('/kakao', passport.authenticate('kakao'))
+router.get('/kakao/callback', passport.authenticate('kakao', {
+    failureRedirect : '/?loginError=카카오로그인 실패',         // passport-kakao 홈페이지 접속해서 양식 그대로 따라하면 됨
+}), (req, res) => {
+    res.redirect('/');
+});
+
 module.exports = router;
